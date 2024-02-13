7e2e38b5c27c04c0bbf30a2277faefb719beb4ee
mahsan <mahsan@thetechfury.com>
2024-02-13 15:09:27 +0500

    add end user login logout functionality

--- core_settings/settings.py
+++ core_settings/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'core_settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
