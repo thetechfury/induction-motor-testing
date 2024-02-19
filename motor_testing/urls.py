b4eed233ebc49b7f28a60d18f7d2ab57133f8e9c
Rana Awais Ahmad <ranaawaix157@gmail.com>
2024-02-19 18:48:07 +0500

    xhtml2pdf integration working in progress...

--- motor_testing/urls.py
+++ motor_testing/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import InductionMotorListingsView, TestsView, ReportView, GeneratePdf
+from .views import InductionMotorListingsView, TestsView, ReportView, GeneratePDF
 
 urlpatterns = [
     path("home/", InductionMotorListingsView.as_view(), name="home"),
     path("tests/<int:pk>", TestsView.as_view(), name="tests"),
     path("report/<int:id>", ReportView.as_view(), name="report"),
-    path('pdf/', GeneratePdf.as_view(), name='pdf'),
+    # path('pdf/<int:id>', GeneratePDF.as_view(), name='pdf'),
 ]
