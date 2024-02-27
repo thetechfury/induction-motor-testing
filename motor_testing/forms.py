e23474d469a6992fc97e802e3f1a2d5fe8050650
mahsan <mahsan@thetechfury.com>
2024-02-27 18:03:10 +0500

    UI improve on performance_determination_test form

--- motor_testing/forms.py
+++ motor_testing/forms.py
@@ -82,61 +82,20 @@ class TemperatureRiseTestForm(forms.ModelForm):
 
 
 class PerformanceDeterminationTestForm(forms.ModelForm):
-    file_1 = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}), required=False)
-    file_2 = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}), required=False)
-    file_3 = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}), required=False)
-    file_4 = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}), required=False)
+    file_1 = forms.FileField()
+    file_2 = forms.FileField()
+    file_3 = forms.FileField()
+    file_4 = forms.FileField()
     prefix = 'performance_determination_test'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "form-control"
 
     class Meta:
         model = PerformanceDeterminationTest
         fields = ['voltage', 'frequency', 'nominal_t', 'file_1', 'file_2', 'file_3', 'file_4']
-        widgets = {
-            'voltage': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Voltage (V)'}),
-            'frequency': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Frequency (Hz)'}),
-            'nominal_t': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Nominal T (N.m)'}),
-            'file_1': forms.FileInput(attrs={'class': 'form-control'}),
-            'file_2': forms.FileInput(attrs={'class': 'form-control'}),
-            'file_3': forms.FileInput(attrs={'class': 'form-control'}),
-            'file_4': forms.FileInput(attrs={'class': 'form-control'}),
-        }
-        labels = {
-            'voltage': 'Voltage (V)',
-            'frequency': 'Frequency (Hz)',
-            'nominal_t': 'Nominal T (N.m)',
-            'file_1': 'File 1 with Load at 25%',
-            'file_2': 'File 2 with Load at 50%',
-            'file_3': 'File 3 with Load at 75%',
-            'file_4': 'File 4 with Load at 100%',
-        }
-
-    def clean_file_1(self):
-        file_1 = self.cleaned_data.get('file_1')
-        if file_1:
-            if not file_1.name.endswith('.xlsx'):
-                raise forms.ValidationError("Only .xlsx files are allowed.")
-        return file_1
-
-    def clean_file_2(self):
-        file_2 = self.cleaned_data.get('file_2')
-        if file_2:
-            if not file_2.name.endswith('.xlsx'):
-                raise forms.ValidationError("Only .xlsx files are allowed.")
-        return file_2
-
-    def clean_file_3(self):
-        file_3 = self.cleaned_data.get('file_3')
-        if file_3:
-            if not file_3.name.endswith('.xlsx'):
-                raise forms.ValidationError("Only .xlsx files are allowed.")
-        return file_3
-
-    def clean_file_4(self):
-        file_4 = self.cleaned_data.get('file_4')
-        if file_4:
-            if not file_4.name.endswith('.xlsx'):
-                raise forms.ValidationError("Only .xlsx files are allowed.")
-        return file_4
 
 
 class NoLoadTestForm(forms.ModelForm):
